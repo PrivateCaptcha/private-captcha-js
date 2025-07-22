5bc25e2572257000b500a1a69d3fa5f34116ed76
Taras Kushnir <tk.dev@mailbox.org>
2025-07-22 18:54:43 +0300

    Bump version also in code

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ export function getStatusCode(error) {
 export const GlobalDomain = 'api.privatecaptcha.com';
 export const EUDomain = 'api.eu.privatecaptcha.com';
 export const DefaultFormField = 'private-captcha-solution';
-const version = '0.0.4'
+const version = '0.0.6'
 const userAgent = 'private-captcha-js/' + version;
 
 const retriableStatusCodes = [
